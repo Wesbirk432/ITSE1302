79a982dcb54c1910312f79b9cef1b9dc4a2a1a6b
Weston Birkhead <wbir913@mail.midland.edu>
2017-09-18 23:15:35 -0500

    Commit Answer 3

--- WEEK_2/week2.groovy
+++ WEEK_2/week2.groovy
@@ -8,3 +8,9 @@ class Constants {
     static final String FIRST_NAME = "Weston"
     static final LAST_NAME = "BIRKHEAD"
 }
+//Com3 - numeric variables
+Integer leet = 1337
+
+Bigdecimal result = leet / 7
+Integer Bigdecimal Float
+print leet - result
