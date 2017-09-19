ccf134aa95e5e203dda7fd746da20935f7947bcb
Weston Birkhead <wbir913@mail.midland.edu>
2017-09-18 23:12:57 -0500

    Commit Answer 2

--- WEEK_2/week2.groovy
+++ WEEK_2/week2.groovy
@@ -2,3 +2,9 @@
 
 //Com1- print hello world
 println "hello world"
+
+//Com2 - constants
+class Constants {
+    static final String FIRST_NAME = "Weston"
+    static final LAST_NAME = "BIRKHEAD"
+}
